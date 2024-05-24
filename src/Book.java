21f858d55779f7a9c5b0f07a01a3bc5f7c7f932b
Azat <vildus@yandex.ru>
2024-05-24 21:37:19 +0300

    ДЗ

---
+++ src/Book.java
@@ -0,0 +1,9 @@
+public class Book {
+    String name;
+    String author;
+    int yearOfPublication;
+
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Book warAndPeace = new Book();
+        warAndPeace.name = "Война и Мир";
+        warAndPeace.author = "Толстой Л.Н.";
+        warAndPeace.yearOfPublication = 1965;
+        System.out.println("warAndPeace.name = " + warAndPeace.name);
+        System.out.println("warAndPeace.author = " + warAndPeace.author);
+        System.out.println("warAndPeace.yearOfPublication = " + warAndPeace.yearOfPublication);
+
+        Book theAdventuresOfTomSawyer = new Book();
+        theAdventuresOfTomSawyer.name = "Приключения Тома Сойера";
+        theAdventuresOfTomSawyer.author = "Марк Твейн";
+        theAdventuresOfTomSawyer.yearOfPublication = 1876;
+        System.out.println("theAdventuresOfTomSawyer.name = " + theAdventuresOfTomSawyer.name);
+        System.out.println("theAdventuresOfTomSawyer.author = " + theAdventuresOfTomSawyer.author);
+        System.out.println("theAdventuresOfTomSawyer.yearOfPublication = " + theAdventuresOfTomSawyer.yearOfPublication);
+
+        Book leComteDeMonteCristo = new Book();
+        leComteDeMonteCristo.name = "Граф Монте-Кристо";
+        leComteDeMonteCristo.author = "Дюма Александр";
+        leComteDeMonteCristo.yearOfPublication = 1844;
+        System.out.println("leComteDeMonteCristo.name = " + leComteDeMonteCristo.name);
+        System.out.println("leComteDeMonteCristo.author = " + leComteDeMonteCristo.author);
+        System.out.println("leComteDeMonteCristo.yearOfPublication = " + leComteDeMonteCristo.yearOfPublication);
 
     }
 }
